d57f59b8a16c235365551c1ea31fbd4ae0ead07f
Akui <a_kui@163.com>
2017-05-27 18:58:26 +0800

    增加一个挑战问题和实现testlist2str

---
+++ other/testlist2str.py
@@ -0,0 +1,21 @@
+#挑战问题
+# 一个文件test1.txt，里面的数据如下：
+# aaa
+# bbb
+# 希望在test12.txt里生成如下数据：
+# https://baidu.com/web/list.jsp?keyword=aaa&cnt=10
+# https://baidu.com/web/list.jsp?keyword=bbb&cnt=10
+
+
+start='https://baidu.com/web/list.jsp?keyword='
+end="&cnt=10"
+fromFile = open('test1.txt','r')
+toFile = open('test12','w')
+lines=fromFile.readlines()
+for item in lines:
+    resultStr=start+item.strip()+end+"\n"
+    toFile.write(resultStr)
+    print(resultStr)
+
+fromFile.close()
+toFile.close()
